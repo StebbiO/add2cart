8630dcd10079af3cd525b1fc45e802f45c7cc722
Stebbi <stefodd@yahoo.com>
2023-06-14 15:47:26 -0500

    add styling

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 Challenge:
 Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
 */
+const addBtnEl = document.getElementById("add-button")
+const inputFieldEl = document.getElementById("input-field")
+
+addBtn.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    
+    console.log(inputValue)
+})
