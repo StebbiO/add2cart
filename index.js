d66713a3d2f5b2c03ee280f04f0c9d947f54f4df
Stebbi <stefodd@yahoo.com>
2023-06-20 15:15:05 -0500

    use onValue to retrieve database values when there is a change

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://realtime-database-b30bd-default-rtdb.firebaseio.com/"
@@ -11,11 +11,34 @@ const shoppingListInDB = ref(database, "shoppingList")
 
 const addBtnEl = document.getElementById("add-button")
 const inputFieldEl = document.getElementById("input-field")
+const shoppingListEl = document.getElementById("shopping-list")
 
 addBtn.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
 
     push(shoppingListInDB, inputValue)
-    
-    console.log(inputValue)
+
+    clearText() 
 })
+
+onValue(shoppingListInDB, function(snapshot) {
+    let itemArray = Object.values(snapshot.val())
+
+    clearShoppingListEl()
+
+    for (let i = 0; i < itemArray.length; i++) {
+        createListEl(itemArray[i])
+    }
+})
+
+function clearShoppingListEl() {
+    shoppingListEl.innerHTML = ""
+}
+
+function clearText() {
+    inputFieldEl.value = ""
+}
+
+function createListEl(itemValue) {
+    shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+}
